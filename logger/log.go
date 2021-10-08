3c887c3b85f8b622ffecbc57b08bf2a85a80026f
hasan <razibul.hasan@brainstation-23.com>
2021-10-08 11:32:29 +0600

    module path update

--- logger/log.go
+++ logger/log.go
@@ -1,7 +1,15 @@
 package logger
 
-import "log"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
-func Println(userID string, requestID string, err error) {
+type Log struct {
+	Context *gin.Context
+}
+
+func (l Log) Println(userID string, requestID string, err error) {
 	log.Printf("[%v:%v] %v", userID, requestID, err)
+	log.Println(l.Context.Request.Header.Get("Authorization"))
 }
